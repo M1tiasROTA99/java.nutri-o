2e4450b13ec0d9f197413d9ce5674cb08b889d06
M1tiasROTA99 <guimaraes.silva.matheus@escola.pr.gov.br>
2025-09-25 08:01:19 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -34,6 +34,7 @@ for (var i = 0; i < pacientes.length; i++) {
     if (peso <= 0 || peso >= 1000) {
         pesoValido = false;
         tdIMC.textContent = "Peso Inválido";
+        paciente.style.backgroundColor = "magenta";
     }
     if (altura <= 0 || altura >= 3.00) {
         alturaValida = false;
