dd35d1a80b4f1a23fa66c679d0f9aa3f0f1394b1
M1tiasROTA99 <guimaraes.silva.matheus@escola.pr.gov.br>
2025-10-02 09:02:14 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -49,3 +49,39 @@ titulo.addEventListener('click', mostraMensagem);
 function mostraMensagem(){
     alert("Saia da cadeira imediatamente Jose Mule");
 }
+
+//acessa o botão
+var BotaoAdicionar = document.querySelector("#adicionar-paciente")
+//Executa os codigos ao clicar no botão
+BotaoAdicionar.addEventListener('click' , function(event){
+    event.preventDefault();
+
+    //acessa o formulario
+    var formulario = document.querySelector("#form-adiciona");
+
+//Captura os valores digitados
+var nome = formulario.nome.value;
+var peso = formulario.peso.value;
+var altura = formulario.altura.value;
+var gordura = formulario.gordura.value;
+
+//cria a tag , <Tr>
+var pacienteTr = document.createElement("Tr");
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
+
+//adiciona os valores
+nomeTd.textContent = nome;
+pesoTd.textContent = peso;
+alturaTd.textContent = altura;
+gorduraTd.textContent = gordura;
+imcTd.textContent = imc;
+
+});
+
+
+
